3e1fe4552a654871053b4fe665a8ab4af1b82d2d
Prydeton <contact@maxwellreid.tech>
2021-03-10 18:40:28 +1100

    Hotfix project img dissapearing

--- src/components/ProjectListing/projectListingStyle.js
+++ src/components/ProjectListing/projectListingStyle.js
@@ -80,8 +80,7 @@ export const ImageContainer = styled.div`
 
     img {
         width: 100%;
-        height: 100%;
-        object-fit: cover;
+        height: auto%;
         display: block;
         margin-left: auto;
         margin-right: auto;
